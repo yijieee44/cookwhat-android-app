f3be2f0286ec6059564581ef8853c7c8b9436c7f
Yuan9896 <52954938+Yuan9896@users.noreply.github.com>
2021-12-10 18:08:47 +0800

    UI for user module (incomplete)

---
+++ CookingApp/app/src/main/java/com/example/cookingapp/UserModule.java
@@ -0,0 +1,14 @@
+package com.example.cookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserModule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_module);
+    }
+}
