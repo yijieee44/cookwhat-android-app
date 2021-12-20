a626b0c8e1a0dff1a2058ba951f9219239098f9d
hongjing1999 <hongjing1999@gmail.com>
2021-12-20 16:14:52 +0800

    merge slide image

---
+++ app/src/main/java/com/example/cookwhat/activities/ViewRecipeActivity.java
@@ -0,0 +1,31 @@
+package com.example.cookwhat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.cookwhat.R;
+
+import java.util.ArrayList;
+
+public class ViewRecipeActivity extends AppCompatActivity {
+
+    ImageSlider imageSlider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_recipe);
+        imageSlider = findViewById(R.id.image_slider);
+        String averylongstring ="wadasdadjkasdnkjasbfjksbdfjksdbfgjksbfgjkfbkjfsdbgjksdbfgkjsd\nasdfbjhasdbfhjsabdfasfasdfhjasbdfs\nafbjahdbsjfbsjhjafbdsfjas";
+        ArrayList<SlideModel> images = new ArrayList<>();
+        images.add(new SlideModel("https://upload.wikimedia.org/wikipedia/commons/9/99/IMG-20200708-WA0006.jpg", averylongstring,null));
+        images.add(new SlideModel("https://upload.wikimedia.org/wikipedia/commons/9/99/IMG-20200708-WA0006.jpg", averylongstring,null));
+        images.add(new SlideModel("https://upload.wikimedia.org/wikipedia/commons/9/99/IMG-20200708-WA0006.jpg", averylongstring,null));
+
+        imageSlider.setImageList(images);
+
+    }
+}
